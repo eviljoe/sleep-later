15fc6327104e77e6af3a941d547777d3dfbfb87f
eviljoe <eviljoe@users.noreply.github.com>
2015-04-04 10:35:10 -0400

    Added license
    Added license to sleep-later.bat

--- sleep-later.bat
+++ sleep-later.bat
@@ -1,1 +1,17 @@
+REM Sleep Later - Put Windows to sleep... later.
+REM Copyright (C) 2015 Joe Falascino
+REM
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 powershell -command "& 'C:\Users\Joe\Desktop\sleep-later.ps1' "
